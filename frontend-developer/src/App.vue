2cb4751628b344536b8744dac722f92a6018e95e
Dzoudzou13 <95144708+Dzoudzou13@users.noreply.github.com>
2024-03-05 01:29:49 +0100

    dokončenie webovej verzie

--- frontend-developer/src/App.vue
+++ frontend-developer/src/App.vue
@@ -10,6 +10,8 @@
 #app {
     margin:  0;  
     font-family: "Mulish", sans-serif;
+    overflow-x: hidden;
+    position: relative;
 }
 </style>
 
--- frontend-developer/src/views/HomeView.vue
+++ frontend-developer/src/views/HomeView.vue
@@ -1,23 +1,26 @@
 <template>
     <div>
         <header>
-            <section class="logo-btn"> 
+            <div class="logo-btn"> 
                 <img src="./../assets/logoR.png" alt="logo" class="logo">
                 <button>Mám záujem o UX audit</button>
-            </section>
-            <section class="title">
+            </div>
+            <div class="title">
                 <h1>Zvýšte svoje zisky optimalizáciou vášho eshopu.</h1>    
-            </section>
+            </div>
         </header>
         <main>
-            <article>
-                <h2>Rýchle výsledky pomocou UX auditu.</h2>
-                <section class="images">
+            <div class="container">
+                <div class="pink">           
+                    <h2>Rýchle výsledky pomocou UX auditu.</h2>
+                    <img src="./../assets/pink.svg" alt="" class="pink1">
+                </div>    
+                <div class="images">
                     <img src="./../assets/premier-google-partner-RGB-search-mobile-vid-disp-shop.jpg" alt="google partner" class="google">
                     <img src="./../assets/memberofadma.png" alt="member of adma" class="adma">
-                </section>
-            </article>
-            <article class="list">
+                </div>
+            </div>
+            <div class="list container">
                 <p>UX auditom vášho riešenia lepšie pochopíte, ako vaši zákazníci používajú váš produkt, <b> zvýšite ich spokojnosť <br> a hlavne vaše zisky.</b></p>
                 <ul>
                     <li>Odhalíme nedostatky na vašej stránke</li>
@@ -26,26 +29,124 @@
                     <li>Znížime vaše náklady na vývoj</li>
                     <li>Zvýšime návštevnosť vašich stránok</li>
                 </ul>
-            </article>
-            <article class="UX">
+            </div>
+            <div class="UX container">
                 <p>Dosiahnite lepšie výsledky vašej webstránky alebo e-shopu <b>pomocou UX auditu.</b></p>
                 <button>Mám záujem o UX audit</button>
-            </article>
+            </div>
 
-            <article class="boxes">
-                <section class="box1">
-                    <p>Pravidelné UX audity a postupné vylepšovanie nášho internetového obchodu je už niekoľko rokov súčasťou našej dlhodobej stratégie v predaji. 
-                        UX tím v Riešeniach odvádza skvelú prácu, ktorú sprevádzajú viditeľné výsledky a spokojnosť našich zákazníkov. 
-                        Okrem ich prístupu k nášmu biznisu … <span>čítať viac</span></p>
-                </section>
-                <section class="box2">
-                    <p>S UX tímom Riešení úzko spolupracujeme už dlhé roky a sú dôležitým partnerom nášho biznisu. 
-                        Vďaka ich know-how pre nás od samého začatku správne nastavili zdravý pomer prvotriedneho dizajnu a funkčnosti webu,
-                        ktorý následne vylepšovali pomocou relevantných analýz a dát. 
-                        Aj ich zásluhou … <span>čítať viac</span></p>
-                </section>
-            </article>
+            <div class="boxes container">
+                <div class="dotte">
+                    <div class="box1">
+                        <p>Pravidelné UX audity a postupné vylepšovanie nášho internetového obchodu je už niekoľko rokov súčasťou našej dlhodobej stratégie v predaji. 
+                            UX tím v Riešeniach odvádza skvelú prácu, ktorú sprevádzajú viditeľné výsledky a spokojnosť našich zákazníkov. 
+                            Okrem ich prístupu k nášmu biznisu … <span>čítať viac</span></p>
+                        <img src="./../assets/dittos.svg" alt="">
+                    </div>
+                </div>
+                <div class="dotte">
+                    <div class="box2">
+                        <p>S UX tímom Riešení úzko spolupracujeme už dlhé roky a sú dôležitým partnerom nášho biznisu. 
+                            Vďaka ich know-how pre nás od samého začatku správne nastavili zdravý pomer prvotriedneho dizajnu a funkčnosti webu,
+                            ktorý následne vylepšovali pomocou relevantných analýz a dát. 
+                            Aj ich zásluhou … <span>čítať viac</span></p>
+                        <img src="./../assets/dittos.svg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="users container">
+                <div class="loga-firiem svet">
+                    <img src="./../assets/svetnapojov.svg" alt="logo firmy SvetNápojov.sk">
+                    <p> <b>Jozef Korman </b> <br> Konateľ  <span>SvetNápojov.sk</span> </p>
+                </div>
+                <div class="loga-firiem">
+                    <img src="./../assets/alaindelon.png" alt="logo firmy  AlainDelon">
+                    <p> <b>Richard Wohlstein </b> <br> Retail manager <span>AlainDelon.sk</span> </p>
+                </div>            
+            </div>
+            <div class="results container">
+                <div class="pink">
+                     <h2>Rýchla a jednoduchá cesta k výsledkom.</h2>
+                     <img src="./../assets/pink.svg" alt="" class="pink2">                
+                    </div>
+            </div>
+            <div class="numbers">
+                <div class="one">
+                    <img src="./../assets/1.svg" alt="čislo 1">
+                    <div class="text-box">
+                        <h3>Ciele auditu</h3>
+                        <p>Určíme si spoločné ciele, nastavíme očakávania a definujeme <b>cieľovú skupinu a jej potreby.</b></p>
+                    </div>
+                    <img class="arrowR" src="./../assets/arrowR.svg" alt="prava šipka">
+                </div>
+                <div class="two">
+                    <img src="./../assets/arrowL.svg" alt="ľava šipka" class="arrowL">
+                    <img src="./../assets/2.svg" alt="čislo 2">
+                    <div class="text-box nmb-2">
+                        <h3>Zbieranie dát</h3>
+                        <p>Zvolíme vhodné metodiky a nástroje na <b>sledovanie správania používateľov a zbieranie dát.</b></p>
+                    </div>
+                </div>
+                <div class="one">
+                    <img src="./../assets/3.svg" alt="čislo 3">
+                    <div class="text-box nmb-3">
+                        <h3>Vyhodnotenie</h3>
+                        <p><b>Vyhodnotíme nazbierané dáta,</b> zosumarizujeme všetky naše zistenia a <b>navrhneme riešenia.</b></p>
+                    </div>
+                    <img class="smallarrowR" src="./../assets/smallerarrowR.svg" alt="prava šipka">
+                </div>
+                <div class="four">
+                    <img src="./../assets/4.svg" alt="čislo 4">
+                    <div class="text-box nmb-2">
+                        <h3>Výsledky auditu</h3>
+                        <p><b>Osobná prezentácia výsledkov a navrhnutých riešení </b> s podrobnou dokumentáciou.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="important container">
+                <div class="pink">
+                    <h2>Zameriame sa na to najdôležitejšie.</h2>
+                    <img src="./../assets/pink3.png" class="pink3" alt="">
+                </div>
+            </div>
+            <div class="ul container">
+                <p>Dosiahnite lepšie výsledky vašej webstránky alebo e-shopu <b>pomocou UX auditu.</b></p>
+                <ul>
+                    <li>Informačnú architektúru a štruktúru kategórií</li>
+                    <li>Správanie návštevníkov na kľúčových stránkach</li>
+                    <li>Rýchlosť a dostupnosť stránok, SEO nastavenia</li>
+                    <li>Váš biznis a vašich zákazníkov</li>
+                    <li>Ciele, ktoré chcete dosiahnuť</li>
+                    <li>Webovú analytiku</li>
+                </ul>
+            </div>
+            <div class="btn container">
+                <button>Mám záujem o UX audit</button>
+            </div>       
         </main>
+        <footer>
+            <div class="text">
+                <h1>Máte otázky? Jednoducho sa spýtajte.</h1>
+            </div>
+            <div class="contact">
+                <a href="tel:+421 (02) 555 65 726" class="tel">+421 (02) 555 65 726 </a>
+                <a href="mailto:uxdesign@riesenia.com" class="email">uxdesign@riesenia.com</a>
+            </div>
+            <div class="company">
+                <img src="./../assets/company.png" alt="RIESENIA">            
+            </div>
+        </footer>
+
+        <div id="modal-form" class="hidden">
+  <div class="modal-content">
+    <h1>Zadajte svoje informácie</h1>
+    <form action="submit.php" method="post">
+      <input type="text" name="name" placeholder="Zadajte svoje meno">
+      <input type="email" name="email" placeholder="Zadajte svoj e-mail">
+      <input type="submit" value="Odoslať">
+    </form>
+  </div>
+</div>
     </div>
 </template>
 
@@ -56,7 +157,7 @@
 header {
     width: 100%;
     height: 34.625em;
-    background: linear-gradient(159deg, #3D2E80 51.38%, #DC1969 125.72%);
+    background: linear-gradient(171deg, #3D2E80 51.38%, #DC1969 125.72%);
 }
 .logo-btn {
     display: flex;
@@ -94,12 +195,12 @@ h1 {
     width: 14.375em;
 }
 
-article {
+.container{
     display: flex;
     justify-content: center;
 }
 
-article h2 {
+.container h2 {
     width: 8.94em;
     height: 4.19em;
     font-size: 3em;
@@ -171,7 +272,7 @@ li {
     margin-top: 0;
 }
 
-button {
+Button {
     width: 19.7em;
     height: 3.5em;
     border-radius: 0.5em;
@@ -182,12 +283,21 @@ button {
     font-size: 1.25em;
     font-weight: 900;
     margin-top: 1em;
+    transition-duration: 2s;
+}
+
+Button:focus,
+Button:hover {
+    background: #EE325C;
+    animation: pulse-169a02ec 1s;
+    box-shadow: inset 0 0 0 2em transparent, 0 0 0 1em transparent;
+    color: #fff;
+    transform: scale(1.1);
 }
 
 .boxes {
     margin-top: 8em;
 }
-
 .box1,
 .box2 {
     width: 26.25em;
@@ -195,9 +305,14 @@ button {
     border: 1px solid #D6D8DD;
 }
 .box1 {
-    margin-right: 9em;
+    margin-right: 6em;
 }
-
+.box1 img,
+.box2 img {
+    position: relative;
+    bottom: 19.4em;
+    left: 2em;
+}
 .box1 p,
 .box2 p {
     width: 22.56em;
@@ -206,10 +321,223 @@ button {
     font-weight: 400;
     line-height: 2em;
     padding: 2em 1.56em 1.75em 2.12em;
+    margin: 0;
 }
 .box1 span,
 .box2 span {
     color: #EE325C;
     font-weight: 700;
+}
+.users {
+    margin-top: 2.06em;
+}
+
+.loga-firiem {
+    display: flex;
+}
+.loga-firiem img {
+    width: 4em;
+    height: 4em;
+}
+.loga-firiem p {
+    padding-left: 1.19em;
+    width: 14.125em;
+    height: 3.06em;
+    line-height: 1.375em;
+    font-size: 1em;
+}
+.loga-firiem b {
+    font-weight: 900;
+}
+.loga-firiem span {
+    color: #EE325C;
+    font-weight: 700;
+}
+
+.svet {
+    margin-right: 6em;
+    position: relative;
+    left: -7em;
+}
+.results h2{
+    width: 10.94em;
+    padding-right: 0em;
+    text-align: center;
+    padding-top: 3.5em;
+}
+
+.one{
+    display: flex;
+    justify-content: center;
+    position: relative;
+    left: -11.8em;
+}
+.text-box {
+    display: flex;
+    width: 22.75em;
+    height: 6.25em;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 3em;
+    padding-top: 1.3em;
+}
+.text-box h3 {
+    font-size: 1.5em;
+    font-weight: 900;
+    line-height: normal;
+    margin: 0;
+}
+.text-box p {
+    font-weight: 400;
+    line-height: 1.75em;
+    margin: 0;
+    width: 22em;
+}
+.arrowR {
+    position: relative;
+    top: 5em;
+    left: 3em;
+    height: 4.375em;
+    width: 4.64em;
+}
+.two {
+    display: flex;
+    justify-content: center;
+    position: relative;
+    left: 12em;
+    padding-top: 3em;
+}
+.nmb-2 p {
+    width: 24em;
+    }
+.arrowL{
+    position: relative;
+    top: 3em;
+    right: 3em;
+}
+.nmb-3 p {
+    width: 24em;
+}
+.smallarrowR {
+    position: relative;
+    top: 4.5em;
+    left: 3em;
+    width: 4.25em;
+    height: 4.31em;
+}
+.four {
+    display: flex;
+    justify-content: center;
+    position: relative;
+    left: 14em;
+    padding-top: 2em;
+}
+
+.important{
+    padding-top: 5em;
+}
+.important h2 {
+    width: 9.6em;
+    padding-right: 10em;
+}
+.ul p {
+    width: 16.69em;
+    height: 4.25em;
+    font-size: 1.625em;
+    font-weight: 400;
+    line-height: 1.5em;
+    margin: 0;
+    margin-right: 2.5em;
+    margin-top: -0.1em;
+}
+.ul ul {
+    width: 27.63em;
+}
+
+.btn{
+    position: relative;
+    top: -5em;
+    left: -17em;
+}
+
+footer{
+    width: 100%;
+    height: 55.1em;
+    background: linear-gradient(161deg, #3D2E80 38.81%, #DC1969 112.05%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.text h1 {
+    width: 18em;
+    font-size: 3em;
+    font-weight: 900;
+    line-height: 1.69em;
+    text-align: center;
+    margin-top: 3em;
+}
+.contact {
+    font-size: 1.875em;
+    font-weight: 700;
+    line-height: 2.5em;
+    margin-top: 4em;
+}
+.tel {
+    color: #fff;
+    text-decoration: none;
+    padding-right: 2em;
+}
+.email{
+   color: #EE325C;
+   text-decoration: none;
+}
+.company {
+    margin-top: 8em;
+    margin-bottom: 7em;
+}
+.pink1{
+    position: relative;
+    top: -9.8em;
+    right: -10em;
+    width: 12.88em;
+}
+.pink2{
+    position: relative;
+    top: -5.5em;
+    left: 13.9em;
+    width: 16.16em;
+}
+.pink3{
+    position: relative;
+    top: -5.5em;
+}
+
+
+
+
+@media screen and (max-width: 1085px ) {
+    .container {
+        flex-direction: column-reverse;
+        align-items: center;
+    }
+    .container h2 {
+        padding-right: 0em;
+    }
+    .list {
+        flex-direction: column;
+        align-items: center;
+    }
+    .list ul {
+        padding-top: 6em;
+    }
+    .list p {
+        width: 18em;
+        margin: 0;
+        left: 0;
+    }
+    .list p br {
+        display: none;
+    }
 }
 </style>
